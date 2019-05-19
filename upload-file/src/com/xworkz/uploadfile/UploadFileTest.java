b1a872031ecc65fdbf5dcfd9df54911e52ccbc08
sonunadaf <msd.nadaf@gmail.com>
2019-05-19 15:40:47 +0530

    commited for stages example

--- upload-file/src/com/xworkz/uploadfile/UploadFileTest.java
+++ upload-file/src/com/xworkz/uploadfile/UploadFileTest.java
@@ -2,9 +2,10 @@ package com.xworkz.uploadfile;
 
 public class UploadFileTest {
 
-	public static void main() {
+	public static void main(String[] args) {
 
 		System.out.println("chages done by me ");
+		// hi
 
 	}
 
